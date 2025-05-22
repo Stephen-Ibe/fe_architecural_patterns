34986578ee5fd7f5fc599982fa903477cb06ab5a
Stephen Ibe <fmy-812@FMY-812-MacBook-Pro.local>
2025-05-23 00:38:40 +0100

    update: fixed redux store and it's types

--- cba-arch/src/store/store.ts
+++ cba-arch/src/store/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { AuthReducer } from "./slices";
 
-export const store = configureStore({
-  reducer: { auth: AuthReducer },
-});
+export const makeStore = () =>
+  configureStore({
+    reducer: { auth: AuthReducer },
+  });
 
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
+export type AppStore = ReturnType<typeof makeStore>;
+export type RootState = ReturnType<AppStore["getState"]>;
+export type AppDispatch = AppStore["dispatch"];
