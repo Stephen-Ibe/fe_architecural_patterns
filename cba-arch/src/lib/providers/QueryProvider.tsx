9d19330a9dc2af1e7211731d050a8f68ec37f75d
Stephen Ibe <fmy-812@FMY-812-MacBook-Pro.local>
2025-05-23 15:27:32 +0100

    update: implemented TanStack

--- cba-arch/src/lib/providers/QueryProvider.tsx
+++ cba-arch/src/lib/providers/QueryProvider.tsx
@@ -1,18 +1,24 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import React, { ReactNode, useRef } from "react";
+import React, { ReactNode, useMemo } from "react";
 
 type Props = { children: ReactNode };
 
 const QueryProvider = ({ children }: Props) => {
-  const queryClient = useRef<QueryClient | null>(null);
-  if (!queryClient.current) {
-    queryClient.current = new QueryClient();
-  }
-
+  const queryConfig = useMemo(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            retry: 3,
+            staleTime: 60 * 60 * 1000, // 1 hour
+            refetchInterval: 30 * 60 * 1000, // 30 minutes
+          },
+        },
+      }),
+    [] // Empty dependency array means this function runs only once on mount
+  );
   return (
-    <QueryClientProvider client={queryClient.current}>
-      {children}
-    </QueryClientProvider>
+    <QueryClientProvider client={queryConfig}>{children}</QueryClientProvider>
   );
 };
 
